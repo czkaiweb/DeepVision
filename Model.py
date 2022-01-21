d87be08a8a3470d9ef72ea63d4d46fd16fc052bd
Kai Wei <czkaiweb@gmail.com>
2022-01-21 01:02:24 -0500

    Day2 small CNN updated

--- Model.py
+++ Model.py
@@ -28,4 +28,34 @@ class CIFAR_NN(nn.Module):
 	def forward(self,x):
 		x = F.relu(self.flatten(x))
 		x = self.logits10(self.dense1(x))
+		#x = F.softmax(x)
+		return x
+
+class CIFAR_CNN(nn.Module):
+	def __init__(self, useBatchNorm = False):
+		super(CIFAR_CNN,self).__init__()
+		self.conv1 = nn.Conv2d(in_channels=3,out_channels=10,kernel_size=3)
+		self.batchNorm1 = nn.BatchNorm2d(10)
+		self.pool1 = nn.MaxPool2d(2)
+		self.flat1 = nn.Flatten()
+		self.dens1 = nn.Linear(15*15*10,100)
+		self.batchNorm2 = nn.BatchNorm1d(100)
+		self.drop  = nn.Dropout(0.1)
+		self.dens2 = nn.Linear(100,10)
+
+
+	def forward(self,x):
+		x = self.conv1(x)
+		if useBatchNorm:
+			x = self.batchNorm1(x)
+		x = F.relu(x)
+		x = self.pool1(x)
+		x = self.flat1(x)
+		x = self.dens1(x)
+		if useBatchNorm:
+			x = self.batchNorm2(x)
+		x = F.relu(x)
+		x = self.drop(x)
+		x = self.dens2(x)
+		#x = F.softmax(x)
 		return x
